d99f5e7a61073eeda0121681914e6b4b7bbf0602
tuongch <49936739+StasJng@users.noreply.github.com>
2024-10-14 11:10:58 +0700

    update

---
+++ MiddleWareTutorial/ServicesCollectionExtensions/CustomMiddleWare.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MiddleWareTutorial.ServicesCollectionExtensions;
+public class CustomMiddleWare : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        await context.Response.WriteAsync("Hello from me");
+    }
+}
+public static class ImplementMiddleWareExtensions
+{
+    public static IApplicationBuilder UseMiddleWare(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CustomMiddleWare>();
+    }
+}
